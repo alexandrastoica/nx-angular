b1617c564128b3036348c58c25237e9bfde9a70d
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-02-01 16:39:54 +0200

    Generate libraries and components

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,14 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreDataModule } from '@ngnx/core-data';
+import { CoreStateModule } from '@ngnx/core-state';
+import { MaterialModule } from '@ngnx/material';
 import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component';
+import { WidgetDetailComponent } from './widgets/widget-detail/widget-detail.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
+  declarations: [AppComponent, WidgetsComponent, WidgetsListComponent, WidgetDetailComponent],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    CoreDataModule,
+    CoreStateModule,
+    HttpClientModule,
+    MaterialModule,
+    StoreModule.forRoot({}, {}),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/dashboard/src/app/widgets/widget-detail/widget-detail.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WidgetDetailComponent } from './widget-detail.component';
+
+describe('WidgetDetailComponent', () => {
+  let component: WidgetDetailComponent;
+  let fixture: ComponentFixture<WidgetDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WidgetDetailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WidgetDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
