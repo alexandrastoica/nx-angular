95a0bdb9ca6699e0f61917ba7402e1a6dd901c8d
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-02-01 17:01:25 +0200

    Add boilerplate code
    Detailed built: https://github.com/alexandrastoica/angular-workshop

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreDataModule } from '@ngnx/core-data';
@@ -7,20 +8,24 @@ import { CoreStateModule } from '@ngnx/core-state';
 import { MaterialModule } from '@ngnx/material';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
 import { WidgetDetailComponent } from './widgets/widget-detail/widget-detail.component';
 import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
 import { WidgetsComponent } from './widgets/widgets.component';
 
 
 @NgModule({
-  declarations: [AppComponent, WidgetsComponent, WidgetsListComponent, WidgetDetailComponent],
+  declarations: [AppComponent, HomeComponent, WidgetsComponent, WidgetsListComponent, WidgetDetailComponent],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     CoreDataModule,
     CoreStateModule,
+    FormsModule,
     HttpClientModule,
     MaterialModule,
+    RoutingModule,
     StoreModule.forRoot({}, {}),
   ],
   providers: [],
--- apps/dashboard/src/app/widgets/widget-detail/widget-detail.component.ts
+++ apps/dashboard/src/app/widgets/widget-detail/widget-detail.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Widget } from '@ngnx/api-interfaces';
 
 @Component({
   selector: 'ngnx-widget-detail',
   templateUrl: './widget-detail.component.html',
   styleUrls: ['./widget-detail.component.scss']
 })
-export class WidgetDetailComponent implements OnInit {
+export class WidgetDetailComponent {
+  currentWidget: Widget;
+  originalTitle = '';
 
-  constructor() { }
+  @Input() set widget(value: Widget) {
+    if(value) this.originalTitle = value.title;
+    this.currentWidget = {...value};
+  };
 
-  ngOnInit(): void {
-  }
-
+  @Output() saved = new EventEmitter;
+  @Output() cancelled = new EventEmitter;
 }
