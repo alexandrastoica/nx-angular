41b3ff6f638147358eec84fa1d7c2c551945543b
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-02-02 14:32:29 +0200

    Pull data from server

--- apps/dashboard/src/app/widgets/widgets.component.ts
+++ apps/dashboard/src/app/widgets/widgets.component.ts
@@ -1,26 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Widget } from '@ngnx/api-interfaces';
-
-const mockWidgets: Widget[] = [
-  { id: '1', title: 'Widget 01', description: 'Pending' },
-  { id: '2', title: 'Widget 02', description: 'Pending' },
-  { id: '3', title: 'Widget 03', description: 'Pending' },
-];
+import { WidgetsService } from '@ngnx/core-data';
+import { Observable } from 'rxjs';
 
 const emptyWidget: Widget = {
   id: null,
   title: '',
   description: '',
-}
+};
 
 @Component({
   selector: 'ngnx-widgets',
   templateUrl: './widgets.component.html',
-  styleUrls: ['./widgets.component.scss']
+  styleUrls: ['./widgets.component.scss'],
 })
 export class WidgetsComponent implements OnInit {
-  widgets: Widget[];
+  widgets$: Observable<Widget[]>;
   selectedWidget: Widget;
+
+  constructor(private widgetsService: WidgetsService) {}
 
   ngOnInit(): void {
     this.reset();
@@ -40,11 +38,11 @@ export class WidgetsComponent implements OnInit {
   }
 
   loadWidgets() {
-    this.widgets = mockWidgets;
+    this.widgets$ = this.widgetsService.all();
   }
 
   saveWidget(widget: Widget) {
-    if(widget.id) {
+    if (widget.id) {
       this.updateWidget(widget);
     } else {
       this.createWidget(widget);
@@ -52,20 +50,20 @@ export class WidgetsComponent implements OnInit {
   }
 
   createWidget(widget: Widget) {
-    const newWidget = Object.assign({}, widget, { id: this.getRandomID()})
-    this.widgets = [...this.widgets, newWidget];
+    const newWidget = Object.assign({}, widget, { id: this.getRandomID() });
+    // this.widgets$ = [...this.widgets$, newWidget];
     this.resetForm();
   }
 
   updateWidget(widget: Widget) {
-    this.widgets = this.widgets.map(w => {
-      return (widget.id === w.id) ? widget : w;
-    });
+    // this.widgets = this.widgets.map((w) => {
+    //   return widget.id === w.id ? widget : w;
+    // });
     this.resetForm();
   }
 
   deleteWidget(widget: Widget) {
-    this.widgets = this.widgets.filter(w => widget.id !== w.id);
+    // this.widgets = this.widgets.filter((w) => widget.id !== w.id);
     this.resetForm();
   }
 
